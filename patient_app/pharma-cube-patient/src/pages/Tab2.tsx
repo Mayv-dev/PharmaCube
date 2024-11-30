b15fc439d0e01d12a0a03410931265424b0a8e05
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2024-11-30 21:27:31 +0000

    Log schedule info from a mock API

--- patient_app/pharma-cube-patient/src/pages/Tab2.tsx
+++ patient_app/pharma-cube-patient/src/pages/Tab2.tsx
@@ -2,7 +2,43 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
+import React, { useEffect,useState } from 'react';
+
+import axios from 'axios';
+
+async function getMockData() {
+  try {
+    const { data, status } = await axios.get(
+      'https://demo3553220.mockable.io/',
+      {
+        headers: {
+          Accept: 'application/json'
+        },
+      },
+    );
+
+    return data;
+
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
+
+getMockData().then(response => console.log(JSON.stringify(response)))
+
 const Tab2: React.FC = () => {
+	const [schedule, setSchedule] = useState<any[]>([]);
+
+  useEffect(() => {
+		getMockData().then(setSchedule);
+  },[]);
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,6 +53,17 @@ const Tab2: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 2 page" />
+        {
+          schedule.map(scheduleItem => {
+            if(scheduleItem.day == 0) return <p key={scheduleItem.id}>Monday   {scheduleItem.time.substring(11,16)}</p>
+            else if(scheduleItem.day == 1) return <p key={scheduleItem.id}>Tuesday   {scheduleItem.time.substring(11,16)}</p>
+            else if(scheduleItem.day == 2) return <p key={scheduleItem.id}>Wednesday   {scheduleItem.time.substring(11,16)}</p>
+            else if(scheduleItem.day == 3) return <p key={scheduleItem.id}>Thursday   {scheduleItem.time.substring(11,16)}</p>
+            else if(scheduleItem.day == 4) return <p key={scheduleItem.id}>Friday   {scheduleItem.time.substring(11,16)}</p>
+            else if(scheduleItem.day == 5) return <p key={scheduleItem.id}>Saturday   {scheduleItem.time.substring(11,16)}</p>
+            else if(scheduleItem.day == 6) return <p key={scheduleItem.id}>Sunday   {scheduleItem.time.substring(11,16)}</p>
+          })
+        }
       </IonContent>
     </IonPage>
   );
