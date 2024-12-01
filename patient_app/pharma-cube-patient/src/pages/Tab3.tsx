1efc271c146efac76cc7932d96336242fd6e5d31
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2024-12-01 00:02:40 +0000

    Log schedule post to a mock API

--- patient_app/pharma-cube-patient/src/pages/Tab3.tsx
+++ patient_app/pharma-cube-patient/src/pages/Tab3.tsx
@@ -2,6 +2,39 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
+import axios from 'axios';
+
+async function postToMockable() {
+  try {
+    const { data, status } = await axios.post(
+      'https://demo3553220.mockable.io/',
+      {
+        id: 12345,
+        day: 0,
+        time: "2024-11-23T17:09:15+00:00"
+      },
+      {
+        headers: {
+          Accept: 'application/json'
+        },
+      },
+    );
+
+    return data;
+
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
+
+postToMockable()
+
 const Tab3: React.FC = () => {
   return (
     <IonPage>
