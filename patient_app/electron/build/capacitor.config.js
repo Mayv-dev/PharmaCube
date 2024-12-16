9ee43117f911d12b49bc587205b397f1b0bb0ac4
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2024-12-16 17:57:21 +0000

    Implement connected SQLite db to project

---
+++ patient_app/electron/build/capacitor.config.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    appId: 'io.ionic.starter',
+    appName: 'PharmaCube Patient',
+    webDir: 'dist'
+};
+exports.default = config;
