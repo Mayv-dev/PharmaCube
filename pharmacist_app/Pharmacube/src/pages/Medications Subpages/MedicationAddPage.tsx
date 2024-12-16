dc37789850a6941050facd711bc54e59a8df8bbf
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2024-12-16 22:07:05 +0000

    Add mock api call to POST medications

--- pharmacist_app/Pharmacube/src/pages/Medications Subpages/MedicationAddPage.tsx
+++ pharmacist_app/Pharmacube/src/pages/Medications Subpages/MedicationAddPage.tsx
@@ -2,11 +2,54 @@ import { useState } from 'react';
 import { IonButton, IonContent, IonPage, IonSelect, IonSelectOption} from '@ionic/react';
 import LowerToolbar from '../../components/LowerToolbar';
 import '../../styles/Medication Subpages/MedicationAddPage.css';
+import axios from 'axios';
+
+
+
 
 const MedicationAddPage: React.FC = () => {
   const [user, setUser] = useState('User');
   const [medication, setMedication] = useState('Medication');
   const [dosage, setDosage] = useState('');
+
+  async function sendToMockable(userInput:string, medicationInput:string, dosageInput:string) {
+   
+    if (userInput != "" && medicationInput != "" && dosageInput != "") {
+      try{
+        console.log("post request being made...")
+        const { data, status } = await axios.post(
+          'https://demo3553220.mockable.io/',
+          {
+            "id": 12345,
+            "user": userInput,  
+            "medication": medicationInput,
+            "dosage amount": dosageInput,
+            "details":"test run"
+          },
+          {
+            headers: {
+              Accept: 'application/json'
+            },
+          },
+        );
+        return data;
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          console.log('error message: ', error.message);
+          return error.message;
+        } else {
+          console.log('unexpected error: ', error);
+          return 'An unexpected error occurred';
+        }
+      }
+    }
+    else {
+      console.log("blank input. Try again")
+    }
+  };
+
+
+
   return(
     <IonPage>
       
@@ -28,7 +71,7 @@ const MedicationAddPage: React.FC = () => {
           <IonSelectOption>TEST 100mg</IonSelectOption>
           <IonSelectOption>TEST 200mg</IonSelectOption>
         </IonSelect>
-        <IonButton>
+        <IonButton onClick={e => sendToMockable(user, medication, dosage)}>
           Give {user} {medication}
         </IonButton>
       </IonContent>
