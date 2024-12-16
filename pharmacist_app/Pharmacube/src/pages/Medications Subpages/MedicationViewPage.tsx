c772e70e4bb774c5e358411511b9e4369ff623bf
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2024-12-16 15:56:07 +0000

    Add basic elements and state to medication view page

--- pharmacist_app/Pharmacube/src/pages/Medications Subpages/MedicationViewPage.tsx
+++ pharmacist_app/Pharmacube/src/pages/Medications Subpages/MedicationViewPage.tsx
@@ -1,15 +1,33 @@
-import { IonContent, IonPage} from '@ionic/react';
+import { IonContent, IonPage, IonSelect, IonSelectOption} from '@ionic/react';
 import LowerToolbar from '../../components/LowerToolbar';
 import '../../styles/Medication Subpages/MedicationViewPage.css';
+import { useState } from 'react';
 
 const MedicationViewPage: React.FC = () => {
+  const [user, setUser] = useState('Unselected');
   return (
     <IonPage>
       
       <LowerToolbar title='View Medications'/>
 
       <IonContent>
+        <p>Select a user</p>
+        <IonSelect placeholder='Users' onIonChange={e => setUser(e.target.value)}>
+          <IonSelectOption>TEST Duffy</IonSelectOption>
+          <IonSelectOption>TEST Murphy</IonSelectOption>
+          <IonSelectOption>TEST McMahon</IonSelectOption>
+        </IonSelect>
 
+        {user == "Unselected" ? null :
+        <>
+          <p>Medications</p>
+          <ul>
+            <li>TEST Medication 1</li>
+            <li>TEST Medication 2</li>
+            <li>TEST Medication 3</li>
+          </ul>
+        </>
+        }
       </IonContent>
 
     </IonPage>
