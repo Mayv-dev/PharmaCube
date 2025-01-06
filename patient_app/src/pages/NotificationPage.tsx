c02adcd16fd7986c62cc040999d71920101701dd
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2025-01-06 16:04:34 +0000

    Use mock API for notification retrieval

--- patient_app/src/pages/NotificationPage.tsx
+++ patient_app/src/pages/NotificationPage.tsx
@@ -1,8 +1,50 @@
 import { IonButton, IonCard, IonContent, IonHeader, IonPage, IonRouterLink, IonTitle, IonToolbar } from '@ionic/react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 //import ExploreContainer from '../components/ExploreContainer';
 import './NotificationPage.css';
 
+enum urgency {
+	LOW, MEDIUM, HIGH
+}
+type Notification = {
+	id:number,
+	content:string,
+	timestamp:string,
+	urgency:urgency
+}
+
+async function getMockData() {
+  try {
+	const { data, status } = await axios.get(
+	  'https://demo3553220.mockable.io/notification',
+	  {
+		headers: {
+		  Accept: 'application/json'
+		},
+	  },
+	);
+
+	return data;
+
+  } catch (error) {
+	if (axios.isAxiosError(error)) {
+	  console.log('error message: ', error.message);
+	  return error.message;
+	} else {
+	  console.log('unexpected error: ', error);
+	  return 'An unexpected error occurred';
+	}
+  }
+}
+
 const NotificationPage: React.FC = () => {
+	const [notifications, setNotifications] = useState<Notification[]>();
+
+	useEffect(() => {
+		getMockData().then(setNotifications);
+	  }, []);
+
   return (
 	<IonPage>
 	  <IonHeader>
@@ -11,7 +53,7 @@ const NotificationPage: React.FC = () => {
 		</IonToolbar>
 	  </IonHeader>
 	  <IonContent fullscreen className='wrapper'>
-		
+		{notifications?.map(notification => <p>{notification.id} {notification.timestamp} {notification.urgency}</p>)}
 	  </IonContent>
 	</IonPage>
   );
