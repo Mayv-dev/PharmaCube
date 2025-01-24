f3f733a366ff89da8aa19c24aa9e5e66c057e762
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2025-01-24 17:37:37 +0000

    Display basic view of regime info from API

---
+++ pharmacist_app/src/components/Regime Components/RegimeItemContainer.tsx
@@ -0,0 +1,89 @@
+import { RegimeItem } from 'api types/types';
+import '../../styles/RegimeItemContainer.css';
+
+function dayConvert(day:number):string {
+	let stringDay = "";
+
+	switch (day) {
+		case 0: {
+			stringDay = "Monday";
+			break;
+		}
+		case 1: {
+			stringDay = "Tuesday";
+			break;
+		}
+		case 2: {
+			stringDay = "Wednesday";
+			break;
+		}
+		case 3: {
+			stringDay = "Thursday";
+			break;
+		}
+		case 4: {
+			stringDay = "Friday";
+			break;
+		}
+		case 5: {
+			stringDay = "Saturday";
+			break;
+		}
+		case 6: {
+			stringDay = "Sunday";
+			break;
+		}
+	}
+
+	return stringDay;
+}
+
+function timeOfDayConvert(day:number):string {
+	let stringDay = "";
+
+	switch (day) {
+		case 0: {
+			stringDay = "Late Night - 00:00-04:00";
+			break;
+		}
+		case 1: {
+			stringDay = "Early Morning - 04:00-08:00";
+			break;
+		}
+		case 2: {
+			stringDay = "Morning - 08:00-12:00";
+			break;
+		}
+		case 3: {
+			stringDay = "Afternoon - 12:00-16:00";
+			break;
+		}
+		case 4: {
+			stringDay = "Evening - 16:00-20:00";
+			break;
+		}
+		case 5: {
+			stringDay = "Night - 20:00-00:00";
+			break;
+		}
+	}
+
+	return stringDay;
+}
+
+const RegimeItemContainer = (regime:RegimeItem) => 
+{
+	return(
+		<div key={regime.id} className='regimeItemContainer'>
+			<p>Compartment: {regime.copartment_id == 0 ? "No compartment, indicated by 0" : regime.copartment_id}</p>
+			<p>Information: {regime.information}</p>
+			<p>Day: {dayConvert(regime.period_scheduled.day)}</p>
+			<p>Instructions: {regime.period_scheduled.instruction}</p>
+			<p>Time of Day: {timeOfDayConvert(regime.period_scheduled.time_period)}</p>
+			<p>Time Offset: {regime.period_scheduled.time_adjustment}</p>
+			<p>Medications: TO BE ADDED TO API DEFINITION</p>
+		</div>
+	)
+}
+
+export default RegimeItemContainer;
--- pharmacist_app/src/pages/Regimes Subpages/ViewRegime.tsx
+++ pharmacist_app/src/pages/Regimes Subpages/ViewRegime.tsx
@@ -1,23 +1,73 @@
 import { useState } from 'react';
+import axios from 'axios';
 import {
   IonPage,
   IonContent,
+  IonSelect,
+  IonSelectOption,
 } from '@ionic/react';
 import '../../styles/Regime Subpages/ViewRegime.css';
 import LowerToolbar from '../../components/LowerToolbar';
+import React from 'react';
+import { RegimeItem } from 'api types/types';
+import RegimeItemContainer from '../../components/Regime Components/RegimeItemContainer';
 
 const ViewRegime = () => {
-  const [user, setUser] = useState('');
-  const [medication, setMedication] = useState('');
-  const [amount, setAmount] = useState('');
-  const [showModal, setShowModal] = useState(false);
-  const [isChecked, setIsChecked] = useState(false);
+	const [userId, setUserId] = useState<number>(123456);
+	const [userName, setUserName] = useState('Unselected');
+	const [userRegimes, setUserRegimes] = useState<RegimeItem[]>()
+
+  async function getMockData() {
+	try {
+	  const { data, status } = await axios.get(
+		'https://demo3553220.mockable.io/user/id/regime',
+		{
+		  headers: {
+			Accept: 'application/json'
+		  },
+		},
+	  );
+  
+	  return data;
+  
+	} catch (error) {
+	  if (axios.isAxiosError(error)) {
+		console.log('error message: ', error.message);
+		return error.message;
+	  } else {
+		console.log('unexpected error: ', error);
+		return 'An unexpected error occurred';
+	  }
+	}
+  }
+
+  const handleUserSelect = (user:string) => {
+    console.log("This should display the medications assigned to: " + user);
+    setUserName(user);
+    getMockData().then(setUserRegimes);
+  }
 
   return (
     <IonPage>
         <LowerToolbar title="Regimes"/>
 			<IonContent className="ion-padding">
-				<p>View Regime</p>
+				<p>Select a user (currently hardcoded)</p>
+				<IonSelect placeholder='Users' onIonChange={e => handleUserSelect(e.target.value)}>
+					<IonSelectOption>TEST Duffy</IonSelectOption>
+					<IonSelectOption>TEST Murphy</IonSelectOption>
+					<IonSelectOption>TEST McMahon</IonSelectOption>
+				</IonSelect>
+			{
+				userName == "Unselected" ? null :
+				<>
+					<p>Medications from API</p>
+					<ul>
+						{/*declaring an object that is passed entirely to the component with ...regime was a solution recieved from the answer of CPHPython 
+						at https://stackoverflow.com/questions/48240449/type-is-not-assignable-to-type-intrinsicattributes-intrinsicclassattribu*/}
+						{userRegimes?.map(regime => <RegimeItemContainer {...regime}/>)}
+					</ul>
+				</>
+			}
 			</IonContent>
     </IonPage>
   );
