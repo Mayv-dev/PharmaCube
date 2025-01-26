8726a29cec4926f83858fe8d07f0fe506dbec1d7
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2025-01-26 23:53:50 +0000

    Match regime form with updated RegimeItem type

--- pharmacist_app/src/api types/types.tsx
+++ pharmacist_app/src/api types/types.tsx
@@ -1,11 +1,9 @@
 export type RegimeItem = {
-	id: number;
-	compartment_id: number; 
-	information: string;
-	period_scheduled: {
-		day:number;
-		time_period:number; //Time period of day, exact time set by user.
-		time_adjustment:number; //Time adjustment before or after period to take
-		instruction:string;
-	}
+    id: number;
+    compartment_id: number; 
+    information: string;
+    day:number;
+    time_period:number; //Time period of day, exact time set by user.
+    time_adjustment:number; //Time adjustment before or after period to take
+    instruction:string;
 };
