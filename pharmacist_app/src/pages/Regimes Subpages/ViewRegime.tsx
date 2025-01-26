f45c74b25693b474abe483d18de674cdb4273ae9
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2025-01-26 01:33:56 +0000

    Add confirmation popup for regime deletion

--- pharmacist_app/src/pages/Regimes Subpages/ViewRegime.tsx
+++ pharmacist_app/src/pages/Regimes Subpages/ViewRegime.tsx
@@ -5,17 +5,25 @@ import {
   IonContent,
   IonSelect,
   IonSelectOption,
+  IonButton,
+  IonHeader,
+  IonModal,
+  IonTitle,
+  IonToolbar,
 } from '@ionic/react';
 import '../../styles/Regime Subpages/ViewRegime.css';
 import LowerToolbar from '../../components/LowerToolbar';
-import React from 'react';
 import { RegimeItem } from 'api types/types';
 import RegimeItemContainer from '../../components/Regime Components/RegimeItemContainer';
 
 const ViewRegime = () => {
 	const [userId, setUserId] = useState<number>(123456);
 	const [userName, setUserName] = useState('Unselected');
+	
 	const [userRegimes, setUserRegimes] = useState<RegimeItem[]>()
+	const [deleteRegimeId, setDeleteRegimeId] = useState<number>(-1);
+
+	const [showModal, setShowModal] = useState(false);
 
   async function getMockData() {
 	try {
@@ -41,8 +49,9 @@ const ViewRegime = () => {
 	}
   }
 
-  async function deleteMockData() {
-	
+  const confirmDeletion = (regimeId:number) => {
+	setDeleteRegimeId(regimeId)
+	setShowModal(true)
   }
 
   const handleUserSelect = (user:string) => {
@@ -51,8 +60,8 @@ const ViewRegime = () => {
     getMockData().then(setUserRegimes);
   }
 
-  const deleteRegimeItem = async (regimeId:number) => {
-	console.log("deleting regime... ", regimeId)
+  const deleteRegimeItem = async () => {
+	console.log("deleting regime... ", deleteRegimeId)
 	try {
 		const { data, status } = await axios.delete(
 		  'https://demo3553220.mockable.io/user/id/regime',
@@ -61,11 +70,12 @@ const ViewRegime = () => {
 			  Accept: 'application/json'
 			},
 			data: {
-				"id":regimeId
+				"id":deleteRegimeId
 			}
 		  }
 		);
 	
+		getMockData().then(setUserRegimes);
 		return data;
 	
 	  } catch (error) {
@@ -77,7 +87,6 @@ const ViewRegime = () => {
 		  return 'An unexpected error occurred';
 		}
 	  }
-    getMockData().then(setUserRegimes);
   }
 
   return (
@@ -96,10 +105,30 @@ const ViewRegime = () => {
 					<ul>
 						{/*declaring an object that is passed entirely to the component with ...regime was a solution recieved from the answer of CPHPython 
 						at https://stackoverflow.com/questions/48240449/type-is-not-assignable-to-type-intrinsicattributes-intrinsicclassattribu*/}
-						{userRegimes?.map(regime => <RegimeItemContainer regime={regime} deleteItem={deleteRegimeItem}/>)}
+						{userRegimes?.map(regime => <RegimeItemContainer regime={regime} deleteItem={confirmDeletion}/>)}
 					</ul>
 				</>
 			}
+
+
+			<IonModal isOpen={showModal} onDidDismiss={() => setShowModal(false)}>
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>Confirm Submission</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent className="ion-padding">
+            <p>Are you sure you wish to delete this regime?</p>
+            <IonButton expand="full" color="primary" onClick={() => {
+				deleteRegimeItem()
+				setShowModal(false)}}>
+              Yes
+            </IonButton>
+            <IonButton expand="full" color="medium" className="cancel-button" onClick={() => setShowModal(false)}>
+              No
+            </IonButton>
+          </IonContent>
+        </IonModal>
 			</IonContent>
     </IonPage>
   );
