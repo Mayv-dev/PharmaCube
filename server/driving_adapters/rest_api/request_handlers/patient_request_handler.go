f3f04c437f37c3b07f8acce08debbcd5e76f20e1
Mayv-dev <91346624+Mayv-dev@users.noreply.github.com>
2025-01-27 17:50:46 +0000

    Patient get regime api

--- server/driving_adapters/rest_api/request_handlers/patient_request_handler.go
+++ server/driving_adapters/rest_api/request_handlers/patient_request_handler.go
@@ -1,7 +1,75 @@
 package requesthandlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+	databaseadapters "pharmacube/server/driven_adapters/database_adapters"
+	"pharmacube/server/driving_adapters/rest_api/responses"
+	"strconv"
 
-func GetPatient(context *gin.Context) {
+	"github.com/gin-gonic/gin"
+)
 
+func GetPatientRegime(context *gin.Context) {
+	patientId, err := strconv.Atoi(context.Param("patient_id"))
+	if err != nil {
+		//Invalid ID
+		log.Println(err.Error())
+		context.JSON(http.StatusBadRequest, responses.ApiResponse{Data: "Invalide Patient ID"})
+		return
+	}
+
+	//Get Patient
+	patient, err := databaseadapters.GetPatient(uint(patientId))
+	if err != nil {
+		//Not found
+		log.Println(err.Error())
+		context.JSON(http.StatusNotFound, responses.ApiResponse{Data: "Patient not Found"})
+		return
+	}
+
+	regime, err := databaseadapters.GetPatientRegime(patient.ID)
+	if err != nil {
+		log.Println(err.Error())
+		context.JSON(http.StatusNotFound, responses.ApiResponse{Data: "Patient Regime not Found"})
+		return
+	}
+
+	context.JSON(http.StatusOK, regime)
+}
+
+func GetPatientRegimeItem(context *gin.Context) {
+	patientId, err := strconv.Atoi(context.Param("patient_id"))
+	if err != nil {
+		//Invalid ID
+		log.Println(err.Error())
+		context.JSON(http.StatusBadRequest, responses.ApiResponse{Data: "Invalide Patient ID"})
+		return
+	}
+
+	regimeId, err := strconv.Atoi(context.Param("patient_id"))
+	if err != nil {
+		//Invalid ID
+		log.Println(err.Error())
+		context.JSON(http.StatusBadRequest, responses.ApiResponse{Data: "Invalide Regime ID"})
+		return
+	}
+
+	//Get Patient
+	patient, err := databaseadapters.GetPatient(uint(patientId))
+	if err != nil {
+		//Not found
+		log.Println(err.Error())
+		context.JSON(http.StatusNotFound, responses.ApiResponse{Data: "Patient not Found"})
+		return
+	}
+
+	regime, err := databaseadapters.GetPatientRegimeItem(patient.ID, uint(regimeId))
+	if err != nil {
+		log.Println(err.Error())
+		context.JSON(http.StatusNotFound, responses.ApiResponse{Data: "Patient Regime not Found"})
+		return
+	}
+
+	context.JSON(http.StatusOK, regime)
 }
--- server/driving_adapters/rest_api/routes/patient_routes.go
+++ server/driving_adapters/rest_api/routes/patient_routes.go
@@ -1,7 +1,12 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	requesthandlers "pharmacube/server/driving_adapters/rest_api/request_handlers"
+
+	"github.com/gin-gonic/gin"
+)
 
 func PatientRoutes(router *gin.Engine) {
-	router.GET("/patient/:id")
+	router.GET("/patient/:patient_id/regime", requesthandlers.GetPatientRegime)
+	router.GET("/patient/:patient_id/regime/:regime_id", requesthandlers.GetPatientRegimeItem)
 }
