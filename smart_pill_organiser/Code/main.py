1090422b6971bccfea3b7a1031adb9a196d4bb05
Jordan-Conway <D00244954@student.dkit.ie>
2025-01-27 22:23:11 +0000

    update code for demo

---
+++ smart_pill_organiser/Code/main.py
@@ -0,0 +1,17 @@
+import MotorController, time
+
+motorcontroller = MotorController.MotorController()
+
+def main():
+    i = 1
+    motorcontroller.start()
+    while(True):
+        motorcontroller.move_to_compartment(i)
+        i+= 2
+        if(i >= 9):
+            i = 0
+        time.sleep(10)
+            
+if __name__ == "__main__":
+    main()
+    
