5daee17b8095657c8e20792d714461e0163a0280
Mayv-dev <91346624+Mayv-dev@users.noreply.github.com>
2025-02-02 18:21:25 +0000

    Fix Database Models

--- server/driving_adapters/rest_api/api_models/regime.go
+++ server/driving_adapters/rest_api/api_models/regime.go
@@ -1,12 +1,10 @@
 package apimodels
 
-import "pharmacube/server/domain"
-
 type Regime struct {
-	CompartmentID int               `json:"compartment_id"`
-	Information   string            `json:"information"`
-	Day           domain.Day        `json:"day"`
-	TimePeriod    domain.TimePeriod `json:"time_period"`
-	TimeOffset    int               `json:"time_adjustment"`
-	Instruction   string            `json:"instruction"`
+	CompartmentID int    `json:"compartment_id"`
+	Information   string `json:"information"`
+	Day           uint   `json:"day"`
+	TimePeriod    uint   `json:"time_period"`
+	TimeOffset    int    `json:"time_adjustment"`
+	Instruction   string `json:"instruction"`
 }
