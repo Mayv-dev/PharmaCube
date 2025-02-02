280f4395eb55ef81c3ae2c6b7a46fb46ff4b94f1
Mayv-dev <91346624+Mayv-dev@users.noreply.github.com>
2025-02-02 16:31:23 +0000

    DB operations for Patient Schedule

---
+++ server/driving_adapters/rest_api/request_handlers/patient_schedule_handler.go
@@ -0,0 +1,7 @@
+package requesthandlers
+
+import "github.com/gin-gonic/gin"
+
+func GetPatientSchedule(context *gin.Context) {
+
+}
--- server/driving_adapters/rest_api/routes/patient_routes.go
+++ server/driving_adapters/rest_api/routes/patient_routes.go
@@ -9,4 +9,10 @@ import (
 func PatientRoutes(router *gin.Engine) {
 	router.GET("/patient/:patient_id/regime", requesthandlers.GetPatientRegime)
 	router.GET("/patient/:patient_id/regime/:regime_id", requesthandlers.GetPatientRegimeItem)
+
+	router.GET("patient/:patient_id/schedule")
+	router.GET("patient/:patient_id/schedule/:schedule_id")
+	router.POST("patient/:patient_id/schedule")
+	router.PUT("patient/:patient_id/schedule/:schedule_id")
+	router.DELETE("patient/:patient_id/schedule/:schedule_id")
 }
