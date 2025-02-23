7eef4df6b1ea69fc704e899ca1e017be1ca1e3b9
Marco <fristtysteam@gmail.com>
2025-02-23 19:40:22 +0000

    Major Color Patch - Marco

--- patient_app/src/pages/SchedulePage.tsx
+++ patient_app/src/pages/SchedulePage.tsx
@@ -1,38 +1,38 @@
-import { IonButton, IonCard, IonContent, IonHeader, IonPage, IonRouterLink, IonTitle, IonToolbar } from '@ionic/react';
-//import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonCard, IonContent, IonHeader, IonIcon, IonPage, IonRouterLink, IonTitle, IonToolbar } from '@ionic/react';
+import { addCircleOutline, createOutline, calendarOutline } from 'ionicons/icons'; // Import the required icons
 import './SchedulePage.css';
 
 const SchedulePage: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle className='center'>Schedule</IonTitle>
+        <IonToolbar color="primary">
+          <IonTitle className="ion-text-center title">
+            PharmaCube Schedule
+          </IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen className='wrapper ion-padding'>
-        <IonCard className='card'>
-        <IonRouterLink routerLink='/ScheduleAddModifyPage'>
-        <IonButton expand="block" className='ScheduleButtons' color="light">
-              Add Item to Schedule
-      </IonButton>
-      </IonRouterLink>
-      <IonRouterLink routerLink='/ScheduleAddModifyPage'>
-        <IonButton expand="block" className='ScheduleButtons' color="light">
-              Modify A Scheduled Item
-      </IonButton>
-      </IonRouterLink>
-      <IonRouterLink routerLink='/ScheduleViewPage'>
-      <IonButton expand="block" className='ScheduleButtons' color="light">
-              View Your Schedule
-      </IonButton>
-      </IonRouterLink>
-      </IonCard>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Schedule</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonContent fullscreen className="ion-padding content" color="secondary">
+        <IonCard className="ion-padding card" color="secondary">
+          <IonRouterLink routerLink="/ScheduleAddModifyPage">
+            <IonButton expand="block" className="ScheduleButtons add-button">
+              <IonIcon icon={addCircleOutline} slot="start" /> {/* Add icon */}
+              <span className="button-text">Add Medication to Schedule</span>
+            </IonButton>
+          </IonRouterLink>
+          <IonRouterLink routerLink="/ScheduleAddModifyPage">
+            <IonButton expand="block" className="ScheduleButtons modify-button">
+              <IonIcon icon={createOutline} slot="start" /> {/* Modify icon */}
+              <span className="button-text">Modify Scheduled Medication</span>
+            </IonButton>
+          </IonRouterLink>
+          <IonRouterLink routerLink="/ScheduleViewPage">
+            <IonButton expand="block" className="ScheduleButtons view-button">
+              <IonIcon icon={calendarOutline} slot="start" /> {/* View icon */}
+              <span className="button-text">View Your Medication Schedule</span>
+            </IonButton>
+          </IonRouterLink>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
