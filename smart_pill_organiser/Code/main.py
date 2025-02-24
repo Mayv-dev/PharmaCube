3a92bdfe630c7a45c9d5ccabefcc169bf21a175f
Jordan-Conway <D00244954@student.dkit.ie>
2025-02-24 22:31:46 +0000

    Rotate lid to correct compartment

--- smart_pill_organiser/Code/main.py
+++ smart_pill_organiser/Code/main.py
@@ -5,9 +5,16 @@ motorcontroller = MotorController.MotorController()
 sh = schedule_handler.Schedule_Handler()
 
 def main():
-    response = sh.get_schedule(1)
-    print(response.json())
+    start()
             
 if __name__ == "__main__":
     main()
-    
+
+def start():
+    while(True):
+        sh.get_schedule()
+        for i in range(0, 6):
+            due = sh.get_due_schedule()
+            for s in due:
+                motorcontroller.move_to_compartment(s[5])
+            time.sleep(10)
