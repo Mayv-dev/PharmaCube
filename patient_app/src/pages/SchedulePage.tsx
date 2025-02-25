ecd126b9b0d252145e27d80e79893a89ea7aaff3
Marco <fristtysteam@gmail.com>
2025-02-25 20:02:08 +0000

    colorblind filter and page changes

--- patient_app/src/pages/SchedulePage.tsx
+++ patient_app/src/pages/SchedulePage.tsx
@@ -1,10 +1,29 @@
-import { IonButton, IonCard, IonContent, IonHeader, IonIcon, IonPage, IonRouterLink, IonTitle, IonToolbar } from '@ionic/react';
-import { addCircleOutline, createOutline, calendarOutline } from 'ionicons/icons'; // Import the required icons
+import {
+  IonCard,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonRouterLink,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonItem,
+  IonLabel,
+  IonSelect,
+  IonSelectOption,
+} from '@ionic/react';
+import { timeOutline, calendarOutline, calendar } from 'ionicons/icons'; 
+import { useState } from 'react';
 import './SchedulePage.css';
 
 const SchedulePage: React.FC = () => {
+  const [filter, setFilter] = useState<string>(''); 
+
   return (
-    <IonPage>
+    <IonPage className={filter}> {}
       <IonHeader>
         <IonToolbar color="primary">
           <IonTitle className="ion-text-center title">
@@ -13,26 +32,47 @@ const SchedulePage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen className="ion-padding content" color="secondary">
-        <IonCard className="ion-padding card" color="secondary">
-          <IonRouterLink routerLink="/ScheduleAddModifyPage">
-            <IonButton expand="block" className="ScheduleButtons add-button">
-              <IonIcon icon={addCircleOutline} slot="start" /> {/* Add icon */}
-              <span className="button-text">Add Medication to Schedule</span>
-            </IonButton>
-          </IonRouterLink>
-          <IonRouterLink routerLink="/ScheduleAddModifyPage">
-            <IonButton expand="block" className="ScheduleButtons modify-button">
-              <IonIcon icon={createOutline} slot="start" /> {/* Modify icon */}
-              <span className="button-text">Modify Scheduled Medication</span>
-            </IonButton>
-          </IonRouterLink>
-          <IonRouterLink routerLink="/ScheduleViewPage">
-            <IonButton expand="block" className="ScheduleButtons view-button">
-              <IonIcon icon={calendarOutline} slot="start" /> {/* View icon */}
-              <span className="button-text">View Your Medication Schedule</span>
-            </IonButton>
-          </IonRouterLink>
-        </IonCard>
+        {/* Filter Selector */}
+        <IonSelect
+          value={filter}
+          placeholder="Select Colorblind Filter"
+          onIonChange={(e) => setFilter(e.detail.value)}
+          className="filter-selector"
+        >
+          <IonSelectOption value="">None</IonSelectOption>
+          <IonSelectOption value="protanopia-filter">Protanopia</IonSelectOption>
+          <IonSelectOption value="deuteranopia-filter">Deuteranopia</IonSelectOption>
+          <IonSelectOption value="tritanopia-filter">Tritanopia</IonSelectOption>
+          <IonSelectOption value="achromatopsia-filter">Achromatopsia</IonSelectOption>
+        </IonSelect>
+
+        {/* Large Schedule Icon */}
+        <div className="center-icon-container">
+          <IonIcon icon={calendar} className="center-icon" />
+        </div>
+
+        {/* Set My Free Times and View Schedule Labels */}
+        <IonGrid className="center-grid">
+          <IonRow className="label-row">
+            <IonCol size="6">
+              <IonItem lines="none" className="card-item">
+                <IonRouterLink routerLink="/ScheduleAddModifyPage" className="card-link">
+                  <IonIcon icon={timeOutline} slot="start" />
+                  <IonLabel className="card-label">Set My Free Times</IonLabel>
+                </IonRouterLink>
+              </IonItem>
+            </IonCol>
+
+            <IonCol size="6">
+              <IonItem lines="none" className="card-item">
+                <IonRouterLink routerLink="/ScheduleViewPage" className="card-link">
+                  <IonIcon icon={calendarOutline} slot="start" />
+                  <IonLabel className="card-label">View Schedule</IonLabel>
+                </IonRouterLink>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
