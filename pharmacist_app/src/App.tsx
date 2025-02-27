bf45fc044f17df48e26dc4cea21a0e1465033016
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2025-02-27 22:14:00 +0000

    Rename Users component to Chat for later feature

--- pharmacist_app/src/App.tsx
+++ pharmacist_app/src/App.tsx
@@ -78,9 +78,19 @@ const App: React.FC = () => {
             </Route>
 
 
+<<<<<<< HEAD
             <Route exact path="/notifications">
               <Notifications />
             </Route>
+=======
+          <Route exact path="/chat">
+            <Chat />
+          </Route>
+          
+          <Route exact path="/account">
+            <Account />
+          </Route>
+>>>>>>> a996a23 (Rename Users component to Chat for later feature)
 
             <Route exact path="/menu">
               <Menu />
