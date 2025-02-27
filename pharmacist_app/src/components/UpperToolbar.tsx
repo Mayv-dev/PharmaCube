b7609b17e4ca2a302de7073b8220b1c9c265936c
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2025-02-27 18:15:08 +0000

    Implement blank notification/hamburger side menus

--- pharmacist_app/src/components/UpperToolbar.tsx
+++ pharmacist_app/src/components/UpperToolbar.tsx
@@ -1,40 +1,45 @@
 import {
-	IonToolbar,
-	IonTitle,
 	IonIcon,
-	IonBadge,
-	IonButton,
-	IonHeader,
-	IonRouterLink,
 	IonContent,
-	IonTabs,
-	IonTab,
 	IonLabel,
 	IonTabBar,
 	IonTabButton,
-	IonRouterOutlet,
-	IonText
+	IonMenu
   } from '@ionic/react';
-  import { medkit,menu,notifications, chatbubbleOutline, calendar, personCircle, map, informationCircle } from 'ionicons/icons';
+  import { menu,notifications } from 'ionicons/icons';
   import '../styles/LowerToolbar.css';
-  import Notifications from '../pages/Notifications';
-  import { Route } from 'react-router';
-  import { IonReactRouter } from '@ionic/react-router';
+  import { menuController } from '@ionic/core/components';
+
+	async function openHamburgerMenu() {
+	  await menuController.open('hamburger-menu');
+	}
   
+	async function openNotificationMenu() {
+	  await menuController.open('notifications');
+	}
+
   const UpperToolbar: React.FC = () => {
 	return (
-	
-	<IonTabBar className='tabBarPrimary' slot="top">
+	<>
+	  <IonTabBar className='tabBarPrimary' slot="top">
+		<IonTabButton tab="menu" onClick={openHamburgerMenu}>
+			<IonIcon icon={menu} aria-hidden="true" />
+			<IonLabel>Menu</IonLabel>
+		</IonTabButton>
+		<IonTabButton tab="notifications" onClick={openNotificationMenu}>
+			<IonIcon icon={notifications} aria-hidden="true" />
+			<IonLabel>Notifications</IonLabel>
+		</IonTabButton>
+	</IonTabBar>
 
-	<IonTabButton tab="menu" href="/menu">
-	  <IonIcon icon={menu} aria-hidden="true" />
-	  <IonLabel>Menu</IonLabel>
-	  </IonTabButton>
-	  <IonTabButton tab="notifications" href="/notifications">
-	  <IonIcon icon={notifications} aria-hidden="true" />
-	  <IonLabel>Notifications</IonLabel>
-	  </IonTabButton>
-  </IonTabBar>
+	  <IonMenu menuId="hamburger-menu" contentId="main-content">
+        <IonContent className="ion-padding">Account and other hamburger menu items will go here.</IonContent>
+      </IonMenu>
+
+      <IonMenu side="end" menuId="notifications" contentId="main-content">
+        <IonContent className="ion-padding">Notifications will go here.</IonContent>
+      </IonMenu>
+	</>
 	);
   };
   
