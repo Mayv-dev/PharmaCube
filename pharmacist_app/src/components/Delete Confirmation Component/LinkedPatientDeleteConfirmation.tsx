6f1bcf6d56ccb14ecb5742946b0f232dc8a96245
Cian Ashby <cianashby1337@gmail.com>
2025-02-27 23:37:10 +0000

    Implement mock linked patient deletion

--- pharmacist_app/src/components/Delete Confirmation Component/LinkedPatientDeleteConfirmation.tsx
+++ pharmacist_app/src/components/Delete Confirmation Component/LinkedPatientDeleteConfirmation.tsx
@@ -1,14 +1,16 @@
 //import ExploreContainer from '../components/ExploreContainer';
-import { IonModal, IonToolbar, IonTitle, IonHeader, IonContent, IonButton } from '@ionic/react';
+import { IonModal, IonToolbar, IonTitle, IonHeader, IonContent, IonButton, IonInput } from '@ionic/react';
+import { useState } from 'react';
 type popupInfo = {
 	patient_id: number;
 	patient_name: string;
-	delete_denied:any;
-	delete_confirmed:any;
+	deletePatient:any;
+	setShowModal:any;
   };
 
-const LinkedPatientDeleteConfirmation: React.FC<popupInfo> = ({patient_id,patient_name,delete_denied,delete_confirmed}) => {
-  return (
+const LinkedPatientDeleteConfirmation: React.FC<popupInfo> = ({patient_id,patient_name, deletePatient, setShowModal}) => {
+	const [deleteInput, setDeleteInput] = useState<string>("")
+	return (
 	<>
 			  <IonHeader>
 				<IonToolbar>
@@ -16,14 +18,17 @@ const LinkedPatientDeleteConfirmation: React.FC<popupInfo> = ({patient_id,patien
 				</IonToolbar>
 			  </IonHeader>
 			  <IonContent className="ion-padding">
-				<p>Are you sure you wish to delete {patient_name} from your list?</p>
-				<IonButton expand="full" color="primary" onClick={() => {
-					//deletePatient()
-					//setShowModal(false)
+				<p>Are you sure you wish to delete {patient_name} from your list? Please enter their name below to confirm deletion.</p>
+				<IonInput value={deleteInput} onIonInput={e => setDeleteInput(e.target.value)}></IonInput>
+				<IonButton expand="full" color={deleteInput == patient_name ? "primary":"dark"} onClick={() => {
+						if(deleteInput == patient_name) {
+							deletePatient()
+							setShowModal(false)
+						}
 					}}>
 				  Yes
 				</IonButton>
-				<IonButton expand="full" color="medium" className="cancel-button" onClick={() => console.log("To be changed") /*setShowModal(false)*/}>
+				<IonButton expand="full" color="medium" className="cancel-button" onClick={() => setShowModal(false)}>
 				  No
 				</IonButton>
 			  </IonContent>
