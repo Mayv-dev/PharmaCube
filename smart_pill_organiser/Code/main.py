5f3e089bb48e55177ac6eb6fcf758030aca10d22
Jordan-Conway <D00244954@student.dkit.ie>
2025-02-27 15:54:36 +0000

    Change wait time for medication to 1 minute

--- smart_pill_organiser/Code/main.py
+++ smart_pill_organiser/Code/main.py
@@ -14,7 +14,8 @@ def start():
     while(True):
         sh.get_schedule()
         for i in range(0, 6):
-            due = sh.get_due_schedule()
+            due = sh.get_due_schedule(time.localtime(), 10)
             for s in due:
                 motorcontroller.move_to_compartment(s[5])
+                time.sleep(60)
             time.sleep(10)
