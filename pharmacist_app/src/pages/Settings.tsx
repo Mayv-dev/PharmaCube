0b6b5c333a795039cc7be93e03330dec10ab62e7
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2025-02-27 21:19:11 +0000

    Create hardcoded elements for settings page

--- pharmacist_app/src/pages/Settings.tsx
+++ pharmacist_app/src/pages/Settings.tsx
@@ -1,11 +1,33 @@
-import { IonContent, IonPage} from '@ionic/react';
+import { IonButton, IonContent, IonItem, IonLabel, IonPage, IonSelect, IonSelectOption, IonText} from '@ionic/react';
 import '../styles/Settings.css';
 
 const Settings: React.FC = () => (
   <IonPage>
 
-    <IonContent>Settings</IonContent>
+    <IonContent>
+      <div className='formBody'>
+        <IonButton>Reset to defaults</IonButton>
 
+        <IonItem>
+          <IonLabel>App theme</IonLabel>
+          <IonSelect>
+            <IonSelectOption>Auto (Device Selected)</IonSelectOption>
+            <IonSelectOption>Dark</IonSelectOption>
+            <IonSelectOption>Light</IonSelectOption>
+          </IonSelect>
+        </IonItem>
+
+        <IonText>Your device's current theme automatically sets the app theme to XXXXXX</IonText>
+
+        <IonItem>
+          <IonLabel>Navbar position</IonLabel>
+          <IonSelect>
+            <IonSelectOption>Top</IonSelectOption>
+            <IonSelectOption>Bottom</IonSelectOption>
+            </IonSelect>
+        </IonItem>
+      </div>
+    </IonContent>
   </IonPage>
 );
 
