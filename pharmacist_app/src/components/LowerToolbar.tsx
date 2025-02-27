d1c4d7428f3d9f8e630beca9e09eaad5411c2030
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2025-02-27 18:15:08 +0000

    Implement blank notification/hamburger side menus

--- pharmacist_app/src/components/LowerToolbar.tsx
+++ pharmacist_app/src/components/LowerToolbar.tsx
@@ -1,36 +1,21 @@
 import {
-	IonToolbar,
-	IonTitle,
 	IonIcon,
-	IonBadge,
-	IonButton,
-	IonHeader,
-	IonRouterLink,
-	IonContent,
-	IonTabs,
-	IonTab,
 	IonLabel,
 	IonTabBar,
-	IonTabButton,
-	IonRouterOutlet,
-	IonText
+	IonTabButton
   } from '@ionic/react';
-  import { menu, medkit, notifications, calendar, personCircle, map, informationCircle } from 'ionicons/icons';
+  import { medkit, chatbubbleOutline } from 'ionicons/icons';
   import '../styles/LowerToolbar.css';
-  import Notifications from '../pages/Notifications';
-  import { Route } from 'react-router';
-  import { IonReactRouter } from '@ionic/react-router';
   
   const LowerToolbar: React.FC = () => {
 	return (
-		
 		<IonTabBar className='tabBarSecondary' slot="top">
 		<IonTabButton tab="regimes" href="/regimes">
 		  <IonIcon icon={medkit} />
 		  <IonLabel>Regimes</IonLabel>
 		</IonTabButton>
 		<IonTabButton tab="chat" href="/chat">
-		  <IonIcon icon={calendar} />
+		  <IonIcon icon={chatbubbleOutline} />
 		  <IonLabel>Chat</IonLabel>
 		</IonTabButton>
 	  </IonTabBar>
