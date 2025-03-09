4c1910299d6a775262e0398553589ee05ab3efe0
Cian Ashby <cianashby1337@gmail.com>
2025-03-09 15:00:33 +0000

    Fix text box background for patient delete

--- pharmacist_app/src/components/Delete Confirmation Component/LinkedPatientDeleteConfirmation.tsx
+++ pharmacist_app/src/components/Delete Confirmation Component/LinkedPatientDeleteConfirmation.tsx
@@ -1,5 +1,5 @@
 //import ExploreContainer from '../components/ExploreContainer';
-import { IonModal, IonToolbar, IonTitle, IonHeader, IonContent, IonButton, IonInput } from '@ionic/react';
+import { IonModal, IonToolbar, IonTitle, IonHeader, IonContent, IonButton, IonInput, IonItem } from '@ionic/react';
 import { useState } from 'react';
 type popupInfo = {
 	patient_id: number;
@@ -18,8 +18,12 @@ const LinkedPatientDeleteConfirmation: React.FC<popupInfo> = ({patient_id,patien
 		</IonToolbar>
 		</IonHeader>
 		<IonContent className="ion-padding">
+			
+		<div className='formBody'>
 		<p>Are you sure you wish to delete {patient_name} from your list? Please enter their name below to confirm deletion.</p>
-		<IonInput value={deleteInput} onIonInput={e => setDeleteInput(e.target.value)}></IonInput>
+		<IonItem>
+			<IonInput value={deleteInput} onIonInput={e => setDeleteInput(e.target.value)}></IonInput>
+		</IonItem>
 		<IonButton expand="full" color={deleteInput == patient_name ? "primary":"dark"} onClick={() => {
 				if(deleteInput == patient_name) {
 					deletePatient()
@@ -31,6 +35,7 @@ const LinkedPatientDeleteConfirmation: React.FC<popupInfo> = ({patient_id,patien
 		<IonButton expand="full" color="medium" className="cancel-button" onClick={() => setShowModal(false)}>
 			No
 		</IonButton>
+		</div>
 		</IonContent>
 	</>
   );
