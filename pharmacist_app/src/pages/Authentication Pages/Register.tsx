05302b0efe24b8cffa70317bf6d9a525ece9ea94
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2025-03-09 13:44:52 +0000

    Implement user test preparations

--- pharmacist_app/src/pages/Authentication Pages/Register.tsx
+++ pharmacist_app/src/pages/Authentication Pages/Register.tsx
@@ -119,6 +119,23 @@ const Register: React.FC = () => {
 
 					<p>Your Pharmacy Details</p>
 					<IonItem>
+						<IonInput onIonChange={e => setPharmacyName(e.target.value)} value={pharmacyName} label='Pharmacy Name'></IonInput>
+					</IonItem>
+
+					<IonItem>
+						<IonInput onIonChange={e => setAddressLine1(e.target.value)} value={addressLine1} label='Address Line 1'></IonInput>
+					</IonItem>
+
+					<IonItem>
+						<IonInput onIonChange={e => setAddressLine2(e.target.value)} value={addressLine2} label='Address Line 2'></IonInput>
+					</IonItem>
+
+					<IonItem>
+						<IonInput onIonChange={e => setAddressLine3(e.target.value)} value={addressLine3} label='Address Line 3'></IonInput>
+					</IonItem>
+
+					<p>Your Pharmacy Details</p>
+					<IonItem>
 						<IonInput onIonChange={e => setPharmacyName(e.target.value)} value={pharmacy_name} label='Pharmacy Name'></IonInput>
 					</IonItem>
 
