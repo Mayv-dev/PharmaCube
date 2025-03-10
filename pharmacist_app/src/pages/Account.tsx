a2ae6ffd76dda4634d40a48c107995646deec887
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2025-03-10 10:59:51 +0000

    Implement front-end CRUD for pharmacist account

--- pharmacist_app/src/pages/Account.tsx
+++ pharmacist_app/src/pages/Account.tsx
@@ -7,23 +7,80 @@ import LinkedPatientDeleteConfirmation from '../components/Delete Confirmation C
 import { string } from 'yup';
 import { useHistory } from 'react-router';
 
+import { PharmacistAccountDetailModify } from '../api types/types';
+
+import axios from 'axios';
+import DeletePharmacistAccountConfirmation from '../components/Delete Confirmation Component/DeletePharmacistAccountConfirmation';
+
 const Account: React.FC = () => {
 	const [isInEditMode, setIsInEditMode] = useState<Boolean>(false)
 	const [patientName, setPatientName] = useState("")
 	const [patientId, setPatientId] = useState(-1)
 	const [showModal, setShowModal] = useState(false)
+	const [showDeletionModal, setShowDeletionModal] = useState(false)
 	const [patientList, setPatientList] = useState(["Ann Murphy", "Aaron Murphy", "Irene Duffy"])
 
+	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const [pharmacyName, setPharmacyName] = useState("");
-	const [addressLine1, setAddressLine1] = useState("");
-	const [addressLine2, setAddressLine2] = useState("");
-	const [addressLine3, setAddressLine3] = useState("");
+	const [pharmacy_name, setPharmacy_name] = useState("");
+	const [pharmacy_address_1, setAddressLine1] = useState("");
+	const [pharmacy_address_2, setAddressLine2] = useState("");
+	const [pharmacy_address_3, setAddressLine3] = useState("");
 	const [postcode, setPostcode] = useState("");
+
 
 	const history = useHistory();
 
+	async function modifyAccount(addedPharmacist:PharmacistAccountDetailModify) {
+		try {
+		  console.log("put request being made...")
+		  const { data, status } = await axios.put(
+			`http://localhost:8080/pharmacist/2`,
+			addedPharmacist,
+			{
+			  headers: {
+				Accept: 'application/json'
+			  },
+			},
+		  );
+		  return data;
+		}
+		catch (error) {
+		  if (axios.isAxiosError(error)) {
+			console.log('error message: ', error.message);
+			return error.message;
+		  } else {
+			console.log('unexpected error: ', error);
+			return 'An unexpected error occurred';
+		  }
+		}
+	  };	
+
+	  const deleteAccount = async () => {
+		let pharmacistId = 2
+		try {
+			const { data, status } = await axios.delete(
+			  `http://localhost:8080/pharmacist/${pharmacistId}`,
+			  {
+				headers: {
+				  Accept: 'application/json'
+				}
+			  }
+			);
+		} catch (error) {
+			if (axios.isAxiosError(error)) {
+				console.log('error message: ', error.message);
+				return error.message;
+			} else {
+				console.log('unexpected error: ', error);
+				return 'An unexpected error occurred';
+			}
+		}
+
+		history.push("/login")
+
+	  }
 
 	const handleModification = async () => {
 		let emailValidation = string().email().required()
@@ -31,18 +88,29 @@ const Account: React.FC = () => {
 			await emailValidation.validate(email).then(result => console.log(result))
 
 			if(
+				name == "" ||
 				email == "" ||
 				password == "" ||
-				pharmacyName == "" ||
-				addressLine1 == "" ||
-				addressLine2 == "" ||
-				addressLine3 == "" ||
+				pharmacy_name == "" ||
+				pharmacy_address_1 == "" ||
+				pharmacy_address_2 == "" ||
+				pharmacy_address_3 == "" ||
 				postcode == ""
 			){
 				console.log("The modification is unacceptable, the incorrect inputs should be highlighted")
 			}
 			else {
 				console.log("The modification is acceptable, the app will now navigate to a confirmation page where the user confirms (along with entering their new email and/or password, only if they changed them)")
+				modifyAccount(
+					{name, 
+					email, 
+					password, 
+					pharmacy_name, 
+					pharmacy_address_1, 
+					pharmacy_address_2, 
+					pharmacy_address_3, 
+					postcode
+					})
 			}
 		}
 		// Figured out how to implement Validation Error while referencing Shanmugaraja_K at https://stackoverflow.com/questions/54649465/how-to-do-try-catch-and-finally-statements-in-typescript
@@ -57,6 +125,10 @@ const Account: React.FC = () => {
 				<div className='formBody'>
 				<p>Your Details</p>
 					<IonItem>
+						<IonInput onIonChange={e => setName(e.target.value)} label='Your Name'></IonInput>
+					</IonItem>
+
+					<IonItem>
 						<IonInput onIonChange={e => setEmail(e.target.value)} type="email" label='Email'></IonInput>
 					</IonItem>
 					
@@ -66,7 +138,7 @@ const Account: React.FC = () => {
 
 					<p>Your Pharmacy Details</p>
 					<IonItem>
-						<IonInput onIonChange={e => setPharmacyName(e.target.value)} label='Pharmacy Name'></IonInput>
+						<IonInput onIonChange={e => setPharmacy_name(e.target.value)} label='Pharmacy Name'></IonInput>
 					</IonItem>
 
 					<IonItem>
@@ -87,6 +159,7 @@ const Account: React.FC = () => {
 
 					<IonButton onClick={e => handleModification()}>Modify Details</IonButton>
 
+					<IonButton color={"danger"} onClick={e => {setShowDeletionModal(true)}}>Delete Your Account</IonButton>
 					<p className='headingText'>Patient list</p>
 					{isInEditMode ? 
 						<IonButton color="danger" onClick={e => setIsInEditMode(false)}>Stop Editing List</IonButton>
@@ -111,6 +184,9 @@ const Account: React.FC = () => {
 
 		<IonModal isOpen={showModal} onDidDismiss={() => setShowModal(false)}>
 			<LinkedPatientDeleteConfirmation patient_id={patientId} patient_name={patientName} setShowModal={setShowModal} deletePatient={() => setPatientList(patientList.filter(patient => patient != patientName))}></LinkedPatientDeleteConfirmation>
+		</IonModal>
+		<IonModal isOpen={showDeletionModal} onDidDismiss={() => setShowDeletionModal(false)}>
+			<DeletePharmacistAccountConfirmation setShowModal={setShowDeletionModal} deleteAccount={() => deleteAccount()}></DeletePharmacistAccountConfirmation>
 		</IonModal>
 		</IonPage>
 	);
