33fea869f10b44f81834309f5feb97c6b8d0db68
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2025-03-17 15:58:04 +0000

    Add initial chat elements, with text-to-speech

---
+++ pharmacist_app/src/components/Chat Components/ChatTextbox.tsx
@@ -0,0 +1,48 @@
+import {
+	IonButton,
+	IonIcon,
+	IonInput,
+	IonLabel,
+	IonTabBar,
+	IonTabButton
+} from '@ionic/react';
+import { musicalNotes, send } from 'ionicons/icons';
+import { useState } from 'react';
+import { boolean } from 'yup';
+
+
+type ChatTextboxProps = {
+	messageSent:any; // could use this to send the go-ahead to update the state above (e.g. the new chat bubble from your message)
+}
+
+
+const ChatTextbox: React.FC<ChatTextboxProps> = ({messageSent}) => {
+	const [messageToSend, setMessageToSend] = useState<string>("");
+
+	const checkProfanity = () => {
+		// return true;
+		// return false;
+	}
+
+	const sendMessage = (messageToSend:string) => {
+		console.log(messageToSend)
+		// return true;
+		// return false;
+	}
+
+	const processMessage = () => {
+		checkProfanity();
+		messageToSend == "" ? null : sendMessage(messageToSend);;
+	}
+
+	return (
+		<div>
+			<IonInput value={messageToSend} placeholder='Enter your message...'></IonInput>
+			<IonButton onClick={() => processMessage()}>
+				<IonIcon icon={send}></IonIcon>
+			</IonButton>
+		</div>
+	);
+};
+
+export default ChatTextbox;
