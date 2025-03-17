56c27873de7d17eb75cb1897db68c2181ebf41f7
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2025-03-17 19:09:17 +0000

    Include profanity filter in pharmacist-sent chats

--- pharmacist_app/src/components/Chat Components/ChatTextbox.tsx
+++ pharmacist_app/src/components/Chat Components/ChatTextbox.tsx
@@ -10,6 +10,7 @@ import { musicalNotes, send } from 'ionicons/icons';
 import { useState } from 'react';
 import { boolean } from 'yup';
 
+import { profanity } from '@2toad/profanity';
 
 type ChatTextboxProps = {
 	messageSent:any; // could use this to send the go-ahead to update the state above (e.g. the new chat bubble from your message)
@@ -19,25 +20,13 @@ type ChatTextboxProps = {
 const ChatTextbox: React.FC<ChatTextboxProps> = ({messageSent}) => {
 	const [messageToSend, setMessageToSend] = useState<string>("");
 
-	const checkProfanity = () => {
-		// return true;
-		// return false;
-	}
+	
 
-	const sendMessage = (messageToSend:string) => {
-		console.log(messageToSend)
-		// return true;
-		// return false;
-	}
-
-	const processMessage = () => {
-		checkProfanity();
-		messageToSend == "" ? null : sendMessage(messageToSend);;
-	}
+	const processMessage = () => messageToSend.length == 0 || profanity.exists(messageToSend) ? null : messageSent(messageToSend);
 
 	return (
 		<div>
-			<IonInput value={messageToSend} placeholder='Enter your message...'></IonInput>
+			<IonInput value={messageToSend} onIonChange={e => setMessageToSend(e.target.value)} placeholder='Enter your message...'></IonInput>
 			<IonButton onClick={() => processMessage()}>
 				<IonIcon icon={send}></IonIcon>
 			</IonButton>
