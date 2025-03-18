8e7d2d9f9f7f47e397bba6253bcbc045315d9178
Cian Ashby <cianashby1337@gmail.com>
2025-03-18 14:11:40 +0000

    Link server history response to calendar dates

--- pharmacist_app/src/pages/History.tsx
+++ pharmacist_app/src/pages/History.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from 'react';
 import { CalendarDate, calendarDateFromJsDateObject, numberOfDaysInMonth } from 'typescript-calendar-date';
 import Calendar from '../components/History Components/Calendar';
 import { PatientAdherenceRecord } from 'api types/types';
+import axios from 'axios';
 
 
 const History: React.FC = () => {
@@ -14,48 +15,7 @@ const History: React.FC = () => {
 	const [dateNow, setDateNow] = useState(calendarDateFromJsDateObject(new Date(Date.now())))
 	const [dateList, setDateList] = useState<any[]>([])
 
-	const [patientHistoricalDosesList, setPatientHistoricalDosesList] = useState<PatientAdherenceRecord[]>([
-		{
-		  "patient_id": 1,
-		  "information": "Aspirin 100mg",
-		  "date_time_scheduled": "2025-03-17T08:00:00Z",
-		  "date_time_taken": "2025-03-17T08:00:00Z",
-		  "was_taken": true,
-		  "time_period": 1
-		},
-		{
-		  "patient_id": 1,
-		  "information": "Metformin 500mg",
-		  "date_time_scheduled": "2025-03-15T12:00:00Z",
-		  "date_time_taken": "2025-03-15T12:00:00Z",
-		  "was_taken": false,
-		  "time_period": 1
-		},
-		{
-		  "patient_id": 1,
-		  "information": "Lisinopril 10mg",
-		  "date_time_scheduled": "2025-03-16T18:00:00Z",
-		  "date_time_taken": "2025-03-16T18:00:00Z",
-		  "was_taken": true,
-		  "time_period": 1
-		},
-		{
-		  "patient_id": 1,
-		  "information": "Lisinopril 10mg",
-		  "date_time_scheduled": "2025-03-16T18:00:00Z",
-		  "date_time_taken": "2025-03-16T18:00:00Z",
-		  "was_taken": false,
-		  "time_period": 1
-		},
-		{
-		  "patient_id": 1,
-		  "information": "Lisinopril 10mg",
-		  "date_time_scheduled": "2025-03-14T18:00:00Z",
-		  "date_time_taken": "2025-03-14T18:00:00Z",
-		  "was_taken": true,
-		  "time_period": 1
-		}
-	  ])
+	const [patientHistoricalDosesList, setPatientHistoricalDosesList] = useState<PatientAdherenceRecord[]>([])
 	
 	const getDateRange = (from: CalendarDate, to: CalendarDate):any[] =>  {
 		let i = from.day
@@ -68,8 +28,37 @@ const History: React.FC = () => {
 		return dateList
 	};
 
+	const getHistoryFromServer = async () => {
+		try {
+		  const { data, status } = await axios.get(
+			`http://localhost:8080/patient/1/history`,
+			{
+			  headers: {
+				Accept: 'application/json'
+			  },
+			  // Got help with handling a 302 return on history GET request with https://stackoverflow.com/questions/67623396/how-to-make-axios-not-to-throw-an-exception-when-http-302-is-encountered-but-re
+			  validateStatus: function (status) {
+                return status == 302;
+            }
+			},
+		  );
+	
+		  return data;
+	
+		} catch (error) {
+		  if (axios.isAxiosError(error)) {
+			console.log('error message: ', error.message);
+			return error.message;
+		  } else {
+			console.log('unexpected error: ', error);
+			return 'An unexpected error occurred';
+		  }
+		}
+	  };
+
 	useEffect(() => {
 		setDateList(getDateRange({ year: date.year, month: date.month, day: 1 }, { year: date.year, month: date.month, day: numberOfDaysInMonth(date)})) 
+		getHistoryFromServer().then(setPatientHistoricalDosesList)
 	},[]);
 
   return(
