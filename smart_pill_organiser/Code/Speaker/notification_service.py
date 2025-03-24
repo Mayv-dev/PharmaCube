44246e68049b2b1ab0ef40bba736523d2dc3eb4d
Jordan-Conway <D00244954@student.dkit.ie>
2025-03-24 19:28:30 +0000

    Add mute option

--- smart_pill_organiser/Code/Speaker/notification_service.py
+++ smart_pill_organiser/Code/Speaker/notification_service.py
@@ -1,14 +1,23 @@
-import smart_pill_organiser.Code.Speaker.audio as audio
+import audio
 import time
+from machine import Pin
 
 class Notification_Service:
     audio = audio.Audio()
+    mute_pin = Pin(9, Pin.IN)
     
     def alert(self):
         for i in range(3):
+            if self.muted():
+                return
             self.audio.write(100, 1)
             time.sleep(1)
             
             
     def confirm(self):
+        if self.muted():
+            return
         self.audio.write(100, 2)
+        
+    def muted(self):
+        return self.mute_pin.value() == 0
