ab569ffce96958fa075557f6b0014b83ccf3783f
Mayv-dev <91346624+Mayv-dev@users.noreply.github.com>
2025-04-19 18:28:33 +0100

    Modified Message Model

--- server/domain/models/message.go
+++ server/domain/models/message.go
@@ -4,9 +4,8 @@ import "time"
 
 type Message struct {
 	Common
-	TimeSent    time.Time `json:"time_sent"`
-	ChatID      uint      `json:"chat_id"`
-	SenderID    uint      `json:"sender_id"`
-	ReceiverID  uint      `json:"receiver_id"`
-	MessageBody string    `json:"message_body"`
+	TimeSent        time.Time `json:"time_sent"`
+	ChatID          uint      `json:"chat_id"`
+	IsSenderPatient bool      `json:"is_send_patient"`
+	MessageBody     string    `json:"message_body"`
 }
