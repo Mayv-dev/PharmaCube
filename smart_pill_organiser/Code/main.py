96d7376e23ed14140a3eb1c41e26911459ceb85f
Jordan-Conway <D00244954@student.dkit.ie>
2025-04-21 12:19:03 +0100

    Add testing features

--- smart_pill_organiser/Code/main.py
+++ smart_pill_organiser/Code/main.py
@@ -3,11 +3,19 @@ import smart_pill_organiser.Code.Schedules.schedule_handler as schedule_handler
 import smart_pill_organiser.Code.Schedules.schedule as schedule
 import smart_pill_organiser.Code.Speaker.notification_service as notification_service
 import time
+from config import TEST_MODE
+from machine import Pin
 
 sh = schedule_handler.Schedule_Handler()
 ns = notification_service.Notification_Service()
 
 def main():
+    if(TEST_MODE):
+        test()
+    else:
+        run()
+
+def run():
     schedule = sh.get_schedule(1)
     for s in schedule:
         print(s)
@@ -27,6 +35,29 @@ def handle_due_schedule(s: schedule.schedule):
         time.sleep(10)
     mc = motor_controller.MotorController()
     mc.start()
+
+def test():
+    turn_pin = Pin(14,Pin.IN)
+    filling_pin = Pin(15, Pin.IN)
+
+    while(True):
+        if turn_pin.value() == 1 :
+            test_medication_due()
+        elif filling_pin.value() == 1:
+            test_guided_filling
+        else:
+            return
+
+def test_medication_due():
+    s = schedule.schedule(time.time(), 4)
+    handle_due_schedule(s)
+
+def test_guided_filling():
+    for i in range (3, 6):
+        mc = motor_controller.MotorController()
+        mc.move_to_compartment(i)
+        mc = motor_controller.MotorController()
+        mc.start()
 
 if __name__ == "__main__":
     main()
