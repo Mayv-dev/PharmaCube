7aa8bde687df540b55268b15e8d2afc4f4e40819
Mayv-dev <91346624+Mayv-dev@users.noreply.github.com>
2025-04-24 23:09:38 +0100

    Removed Time Sent

--- server/domain/models/message.go
+++ server/domain/models/message.go
@@ -1,11 +1,8 @@
 package models
 
-import "time"
-
 type Message struct {
 	Common
-	TimeSent        time.Time `json:"time_sent"`
-	ChatID          uint      `json:"chat_id"`
-	IsSenderPatient bool      `json:"is_sender_patient"`
-	MessageBody     string    `json:"message_body"`
+	ChatID          uint   `json:"chat_id"`
+	IsSenderPatient bool   `json:"is_sender_patient"`
+	MessageBody     string `json:"message_body"`
 }
