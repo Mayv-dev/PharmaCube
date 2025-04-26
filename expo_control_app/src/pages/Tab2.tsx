0044818428ae10e19531239e129dcd7506dd65f7
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2025-04-27 00:15:24 +0100

    Create expo control app

---
+++ expo_control_app/src/pages/Tab2.tsx
@@ -0,0 +1,14 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab2.css';
+
+const Tab2: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
