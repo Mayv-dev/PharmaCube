6f2e758470eaf7d0377d5b29b8c5bd66a3aa1711
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2025-04-27 01:37:00 +0100

    Implement QR-assisted account linking

--- expo_control_app/capacitor.config.ts
+++ expo_control_app/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.expo.control',
   appName: 'expo_control_app',
   webDir: 'dist'
 };
